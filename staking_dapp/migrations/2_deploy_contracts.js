23fc63d6432f0a19d67be0dff63e7d7a03aed821
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2021-12-02 10:27:36 +0000

    migrations: Add deployment script for contracts

---
+++ staking_dapp/migrations/2_deploy_contracts.js
@@ -0,0 +1,25 @@
+const Dummy_Token = artifacts.require("Dummy_Token");
+const Tether_Token = artifcats.require("Tether_Token");
+const Staking_Dapp = artifacts.require("Staking_Dapp");
+
+module.exports = async function(deployer, network, accounts){
+
+	// Deploy Tether Contract
+	await deployer.deploy(Tether_Token)
+	const tether_token = await Tether_Token.deployed()
+
+	// Deploy Dummy Contract
+	await deployer.deploy(Dummy_Token)
+	const dummy_token = await Dummy_Token.deployed()
+	
+	// Deploy Staking Dapp Contract
+	await deployer.deploy(Staking_Dapp, dummy_token.address, tether_token.address)
+	const staking_dapp = await Staking_Dapp.deployed()
+
+	// Transfer Dummy tokens to the staking dapp so it can reward stakers
+	await dummy_token.transfer(staking_dapp.address, '1000000000000000000000000')
+
+	// Transer Tether tokens to the 1st Ganache account
+	await tether_token.transfer(accounts[1], '1000000000000000000000000')
+
+}
